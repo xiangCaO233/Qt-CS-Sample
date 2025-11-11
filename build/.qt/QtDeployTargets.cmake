96b6f5923eedea3208ade648c56c0fcc9bc1fede
xiangCaO233 <odokoei@outlook.com>
2025-11-11 09:00:29 +0800

    style(directory): 修复仓库多余build目录 (#2)
    * 移除build目录
    
    * 更新ignore

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,4 +0,0 @@
-set(__QT_DEPLOY_TARGET_qtip-ServerTest_FILE /home/xiang/Documents/coding/c_cpp/qtip/build/qtip-ServerTest)
-set(__QT_DEPLOY_TARGET_qtip-ServerTest_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_qtip-ClientTest_FILE /home/xiang/Documents/coding/c_cpp/qtip/build/qtip-ClientTest)
-set(__QT_DEPLOY_TARGET_qtip-ClientTest_TYPE EXECUTABLE)
