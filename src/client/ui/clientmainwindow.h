20851e53001087ac7c8c3045246e702f3cb81788
xiangCaO233 <odokoei@outlook.com>
2025-11-12 14:03:30 +0800

    feat(ui): 更新基本ui连接断开逻辑 (#8)

--- src/client/ui/clientmainwindow.h
+++ src/client/ui/clientmainwindow.h
@@ -7,6 +7,8 @@ namespace Ui {
 class ClientMainWindow;
 }
 
+class StandardClient;
+
 class ClientMainWindow : public QMainWindow {
     Q_OBJECT
 
@@ -14,8 +16,14 @@ class ClientMainWindow : public QMainWindow {
     explicit ClientMainWindow(QWidget *parent = nullptr);
     ~ClientMainWindow();
 
+   private slots:
+    void on_connect_button_clicked();
+
+    void on_disconnect_button_clicked();
+
    private:
     Ui::ClientMainWindow *ui;
+    StandardClient *m_Client;
 };
 
 #endif  // CLIENTMAINWINDOW_H
